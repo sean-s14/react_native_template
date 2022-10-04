f40077bc91e8c696b7294a7943e6d0c41fb4a59a
Shaun Stocker <s.stocker04@outlook.com>
2022-10-04 23:36:09 +0100

    Added bottom navigation with react-navigation

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { HomePage } from '~/src/pages/exports';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+import { 
+  HomePage, 
+  AboutPage 
+} from '~/src/pages/exports';
+
+const Tab = createMaterialBottomTabNavigator();
 
 const App = () => {
 
   return (
-    <HomePage />
+    // <HomePage />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen name="About" component={AboutPage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );  
 }
 
